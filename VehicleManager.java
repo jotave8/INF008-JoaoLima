d265421237ce8d075b620f9e0b20ad08eb81d651
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-05-04 22:44:02 +0000

    update

--- VehicleManager.java
+++ VehicleManager.java
@@ -1,4 +1,4 @@
-public class Vehicle(){
+public class Vehicle{
     private String plate;
     private String model;
     private int year;
@@ -35,10 +35,41 @@ public class Vehicle(){
         this.year = year;
     }
 
-    public abstract void displayDetails(); 
-
-    public static void  getTotalVehicles(){
+    public static int getTotalVehicles(){
         return totalVehicles;
     }
+
+    public abstract void displayDetails(); 
     public abstract double calculateDaily(int dias);
+}
+
+public class Car extends Vehicle{
+    private int numberDoors;
+
+    public Car(String plate, String model, int year, int numberDoors){
+        super(plate, model, year);
+        this.numberDoors = numberDoors;
+    }
+
+    @Override
+    public calculateDaily(int dias){
+        return (dias * 100.0) + (numberDoors * 20.0);
+    }
+
+    @Override
+    public abstract void displayDetails(){
+        System.out.println(
+            "Carro - Placa: " + getPlate() +
+            ", Modelo: " + getModel() +
+            ", Ano: " + getYear() +
+            ", Portas: " + numberDoors
+       );
+    }
+
+    public int getNumberDoor(){
+        return numberDoors;
+    }
+    public void setNumberDoor(int numberDoors){
+        this.numberDoors = numberDoors;
+    }
 }
