8c900d2daac7558fc85a60cd147607b51230c6e5
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-05-05 17:41:09 +0000

    update RentalCompany

--- VehicleManager.java
+++ VehicleManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 abstract class Vehicle{
     private String plate;
     private String model;
@@ -102,5 +104,36 @@ class Motorcycle extends Vehicle{
     }
     public void setCylinder(int cylinder){
         this.cylinder = cylinder;
+    }
+}
+
+class RentalCompany{
+    private String name;
+    private ArrayList<Vehicle> registeredVehicle;
+
+
+    public RentalCompany(String name){
+        this.name = name;
+        this.registeredVehicle = new ArrayList<>();
+    }
+
+    public void registerVehicle (Vehicle v){
+        registeredVehicle.add(v);
+    }
+
+    public void listVehicle(){
+        for (Vehicle vehicle : registeredVehicle){
+            vehicle.displayDetails();
+        }
+    }
+
+    public double calculateTotalRentalValue(String plate, int days){
+        for (Vehicle vehicle : registeredVehicle){
+            if (vehicle.getPlate().equals(plate)){
+                return vehicle.calculateDaily(days);
+            }
+        }
+        System.out.println("Veículo com placa " + plate + " não encontrado!");
+        return -1.0;
     }
 }
