003cf942bfae676a451dc8b734952bde28b4add7
jotave8 <jvol180908@gmail.com>
2025-05-29 11:46:32 -0300

    update

--- Work1/src/main/java/com/events/Event.java
+++ Work1/src/main/java/com/events/Event.java
@@ -1,5 +1,22 @@
 package com.events;
 
-class Event{
-    
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+abstract class Event{
+    private String title;
+    private String location;
+    private String description;
+    private int capacity;
+    private LocalDate date;
+    private ArrayList<Participant> participants = new ArrayList<>();
+
+    public Event(String title, String location, String description, int capacity, LocalDate date){
+        this.title = title;
+        this.location = location;
+        this.description = description;
+        this.capacity = capacity;
+        this.date = date;
+    }
 }
--- Work1/src/main/java/com/events/Participant.java
+++ Work1/src/main/java/com/events/Participant.java
@@ -1,5 +1,43 @@
 package com.events;
 
-class Participant{
-    
+abstract class Participant{
+    private String name;
+    private String email;
+
+    public Participant(String name, String email){
+        this.name = name;
+        this.email = email;
+    } 
+
+    public String getName(){    
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
+
+public class Student extends Participant{
+    private int studentId;
+
+    public Student(String name, String email, int studentId){
+        super(name,email);
+        this.studentId = studentId;
+    }
+}
+
+public class Teacher extends Participant{
+    private int teacherId;
+
+    public Teacher(String name, String email, int teacherId){
+        super(name, email);
+        this.teacherId;
+    }
 }
